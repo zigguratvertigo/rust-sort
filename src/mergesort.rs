8cf34e8bf4955058d0d3164bc462ec55c38cb295
Johan Andersson <repi@repi.se>
2017-12-31 13:47:29 +0800

    :hammer: Fixed so merge sort supports arbitrary sized containers

--- src/mergesort.rs
+++ src/mergesort.rs
@@ -19,32 +19,35 @@ use std::cmp::min;
 // 1. Divide the unsorted list into n-sublists, each containing 1 element. A list of 1 element is considered sorted.
 // 2. Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining.
 //    This will be the sorted list.
-fn bottom_up_merge_sort(a: &mut [u32; NUM_VALUES]) {
+fn bottom_up_merge_sort(a: &mut [u32]) {
     // Initialize our work array
-    let mut b: [u32; NUM_VALUES] = [0; NUM_VALUES];
+    let mut b = vec![0u32; a.len()];
+
+    let len = a.len();
 
     let mut width = 1;
-    while width < NUM_VALUES {
+    while width < len {
         // Array A is full of runs of length width.
         let mut i = 0;
-        while i < NUM_VALUES {
+        while i < len {
             // Merge two runs: a[i:i+width-1] nd a[i+width:i+2*width-1] to b[]
             // or copy a[i:n-1] to b[] if(i+width >= n)
             bottom_up_merge(
                 a,
                 i,
-                min(i + width, NUM_VALUES),
-                min(i + 2 * width, NUM_VALUES),
-                &mut b,
+                min(i + width, len),
+                min(i + 2 * width, len),
+                b.as_mut_slice(),
             );
             i = i + 2 * width;
         }
 
         // Now work array B is full of runs of length 2*width.
         // Copy array B to array A for next iteration.
-        for i in 0..NUM_VALUES {
+        for i in 0..len {
             a[i] = b[i];
         }
+
         // Now array A is full of runs of length 2*width.
 
         // Make successively longer sorted runs of length 2, 4, 8, 16... until whole array is sorted.
@@ -53,11 +56,11 @@ fn bottom_up_merge_sort(a: &mut [u32; NUM_VALUES]) {
 }
 
 fn bottom_up_merge(
-    a: &mut [u32; NUM_VALUES],
+    a: &mut [u32],
     i_left: usize,
     i_right: usize,
     i_end: usize,
-    b: &mut [u32; NUM_VALUES],
+    b: &mut [u32],
 ) {
     let mut i = i_left;
     let mut j = i_right;
