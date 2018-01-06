2f42ec42e43d3049983d7a27a3c11375fb94f253
Colin Barré-Brisebois <colinbb@gmail.com>
2018-01-06 16:32:47 +0100

    :snowflake: Added tests
    - Validates that the outputs of all sorts match.
    - Added tests script to travis.

--- src/main.rs
+++ src/main.rs
@@ -48,3 +48,32 @@ fn main() {
     quicksort::sort(&mut result_quicksort);
     println!("{:?}\n", &result_quicksort);
 }
+
+#[test]
+fn sort_results_match() {
+    // Initialize the array values to random numbers
+    let values: Vec<u32> = (0..NUM_VALUES)
+        .map(|_| Range::new(0, MAX_RANGE).ind_sample(&mut rand::thread_rng()))
+        .collect();
+
+    let mut result_bubblesort: Vec<u32> = values.clone();
+    bubblesort::sort(&mut result_bubblesort);
+
+    let mut result_insertionsort: Vec<u32> = values.clone();
+    insertionsort::sort(&mut result_insertionsort);
+
+    let mut result_mergesort: Vec<u32> = values.clone();
+    mergesort::sort(&mut result_mergesort);
+
+    let mut result_selectionsort: Vec<u32> = values.clone();
+    selectionsort::sort(&mut result_selectionsort);
+
+    let mut result_quicksort: Vec<u32> = values.clone();
+    quicksort::sort(&mut result_quicksort);
+
+    // test results by transitivity (A == B == C == D)
+    assert!(result_bubblesort == result_insertionsort);
+    assert!(result_insertionsort == result_mergesort);
+    assert!(result_mergesort == result_selectionsort);
+    assert!(result_selectionsort == result_quicksort);
+}
